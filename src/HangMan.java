81f57e43dca5394b93da41613aa02bb66651e593
Burk Friel <burk.friel.2022@student.lhschools.org>
2018-11-02 14:14:36 -0400

    start

---
+++ src/HangMan.java
@@ -0,0 +1,17 @@
+public class HangMan {
+
+    public static void main(String[] largs) {
+
+        String guessedletters;
+        int numberofwords;
+        int numberofletters;
+        int numberofmisses;
+        
+
+    }
+
+
+
+
+
+}
