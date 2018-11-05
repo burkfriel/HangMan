28e5514995d8741381ce619e851a3147fd29e0a1
Burk Friel <burk.friel.2022@student.lhschools.org>
2018-11-05 14:23:27 -0500

    work on

--- src/HangMan.java
+++ src/HangMan.java
@@ -5,13 +5,82 @@ public class HangMan {
         String guessedletters;
         int numberofwords;
         int numberofletters;
-        int numberofmisses;
-        
+        int numberoftries;
+        numberoftries = 6;
+        numberofwords = 3;
+
+
+
+        System.out.println("This is a Hang man game");
+
+        for (int i =0; 1 < 6; i++){
+            displayHangman(i);
+        }
+
+
+
+
+
+
+    }
+    public static int display (Scanner keyboard){
+        switch (keyboard.nextInt()){
+            case 1:
+                return 1;
+            case 2:
+                ruturn 2;
+        }
 
     }
 
+    public static void displayHangman(int tries){
+        switch (tries){
+            case 0:
+                System.out.println("  +---------");
+                System.out.println("           |");
+                System.out.println("           |");
+                System.out.println("           |");
+                break;
+            case 1:
+                System.out.println("  +---------");
+                System.out.println("  0        |");
+                System.out.println("           |");
+                System.out.println("           |");
+                break;
+            case 2:
+                System.out.println("  +---------");
+                System.out.println("  0        |");
+                System.out.println("  |        |");
+                System.out.println("           |");
+                break;
+            case 3:
+                System.out.println("  +---------");
+                System.out.println("  0        |");
+                System.out.println("  |        |");
+                System.out.println(" /         |");
+                break;
+            case 4:
+                System.out.println("  +---------");
+                System.out.println("  0        |");
+                System.out.println("  |        |");
+                System.out.println(" / \\      |");
+                break;
+            case 5:
+                System.out.println("  +---------");
+                System.out.println("\\ 0       |");
+                System.out.println("  |        |");
+                System.out.println(" / \\      |");
+                break;
+            case 6:
+                System.out.println("  +---------");
+                System.out.println("\\ 0 /     |");
+                System.out.println("  |        |");
+                System.out.println(" / \\      |");
+                break;
 
 
+        }
+    }
 
 
 }
