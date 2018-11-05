bbbd18884a416a442130c1eb3fcdb55f6409ee1d
burkfriel <42781365+burkfriel@users.noreply.github.com>
2018-11-05 15:30:12 -0500

    Alpha 1

--- src/HangMan.java
+++ src/HangMan.java
@@ -28,7 +28,15 @@ public class HangMan {
             case 1:
                 return 1;
             case 2:
-                ruturn 2;
+                return 2;
+            case 3:
+                return 3;
+            case 4:
+                return 4;
+            case 5:
+                return 5;
+            case 6:
+                return 6;
         }
 
     }
